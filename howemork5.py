2b565e0510c79a75139b155f149b33a21ab41586
ChangingChunk <38633470+ChangingChunk@users.noreply.github.com>
2024-04-27 13:54:28 +0300

    Add files via upload

---
+++ howemork5.py
@@ -0,0 +1,13 @@
+my_list = ['банан' , 'апельсин' , 'яблоко' , 'слива' , 'груша']
+print('List:' , my_list)
+print('First element:', my_list[0])
+print('Last element:', my_list[-1])
+print('Sublist:', my_list[2:5])
+my_list[2] = 'дыня'
+print('Modified list:', my_list)
+print('')
+my_dict = {'fruits' : 'фрукты' , 'meat' : 'мясо' , 'vegetables' : 'овощи'}
+print('Dictionary:', my_dict)
+print('Translation:', my_dict['meat'])
+my_dict['fruits'] = 'не фрукты'
+print(my_dict)
